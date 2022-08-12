eb564e2c51b39ee0dce2ad0f6c099611cce73d4e
LITTLESITE <108331059+LITTLESITE@users.noreply.github.com>
2022-08-12 16:10:13 +0800

    Update get.sh

--- get.sh
+++ get.sh
@@ -10,6 +10,12 @@ fi
 if [ ! -f "./sub_v2rayshare.yaml" ];then
  rm ./sub_v2rayshare.txt
 fi
+if [ ! -f "./allmysub.yaml" ];then
+ rm ./allmysub.yaml
+fi
+if [ ! -f "./allmysub.txt" ];then
+ rm ./allmysub.txt
+fi
 echo get subscribe
 wget https://v2rayshare.com/wp-content/uploads/$currentyears/$currentmonths/$currentdate.txt
 wget https://v2rayshare.com/wp-content/uploads/$currentyears/$currentmonths/$currentdate.yaml
@@ -17,6 +23,8 @@ echo get subscribe sucessfully
 mv $currentdate.txt sub.txt
 mv $currentdate.yaml sub.yaml
 wget https://short.littlesite.ml/7kPjan
-mv 7kPjan allmysub.txt
+mv ./7kPjan allmysub.yaml
+wget https://short.littlesite.ml/zQCe3c
+mv ./zQCe3c allmysub.txt
 echo hope you have a good day~
 echo bye~
