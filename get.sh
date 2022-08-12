d42ad984e6b6db37f66d5263fb763ec29e2fe28f
LITTLESITE <108331059+LITTLESITE@users.noreply.github.com>
2022-08-12 15:10:04 +0800

    Update get.sh

--- get.sh
+++ get.sh
@@ -1,22 +1,26 @@
 currentdate=$(date +%Y%m%d)  
 currentmonth=$(date +%Y%m)
+currentmonths=$(date +%m)
+currentyears=$(date +%Y)
 cd `dirname $0`
 echo delete old subscribes
-if [ ! -f "./sub.txt" ];then
- rm ./sub.txt
+if [ ! -f "./sub_nodefree.txt" ];then
+ rm ./sub_nodefree.txt
 fi
-if [ ! -f "./sub.yaml" ];then
- rm ./sub.yaml
+if [ ! -f "./sub_v2rayshare.txt" ];then
+ rm ./sub_v2rayshare.txt
 fi
 echo start download the subscribes
-echo https://nodefree.org/dy/$currentmonth/$currentdate.yaml
-wget https://nodefree.org/dy/$currentmonth/$currentdate.yaml
-echo get yaml sucessfully
-echo https://nodefree.org/dy/$currentmonth/$currentdate.txt
+echo nodefree
 wget https://nodefree.org/dy/$currentmonth/$currentdate.txt
-echo get txt sucessfully
+echo get subscribe sucessfully
 echo start rename the file~
-mv $currentdate.yaml sub.yaml
-mv $currentdate.txt sub.txt
+mv $currentdate.txt sub_nodefree.txt
+
+echo v2rayshare
+wget https://v2rayshare.com/wp-content/uploads/$currentyears/$currentmonths/$currentdate.txt
+echo get subscribe sucessfully
+mv $currentdate.txt sub_v2rayshare.txt
+
 echo hope you have a good day~
 echo bye~
