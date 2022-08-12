ff2fd66d15af2b1cb2ec44839d3df56be8c52e5f
LITTLESITE <108331059+LITTLESITE@users.noreply.github.com>
2022-08-12 15:24:37 +0800

    Update get.sh

--- get.sh
+++ get.sh
@@ -16,20 +16,11 @@ fi
 if [ ! -f "./sub_v2rayshare.yaml" ];then
  rm ./sub_v2rayshare.txt
 fi
-echo start download the subscribes
-echo nodefree
-wget https://nodefree.org/dy/$currentmonth/$currentdate.txt
-wget https://nodefree.org/dy/$currentmonth/$currentdate.yaml
-echo get subscribe sucessfully
-echo start rename the file~
-mv $currentdate.txt sub_nodefree.txt
-mv $currentdate.yaml sub_nodefree.yaml
-
-echo v2rayshare
+echo get subscribe
 wget https://v2rayshare.com/wp-content/uploads/$currentyears/$currentmonths/$currentdate.txt
 wget https://v2rayshare.com/wp-content/uploads/$currentyears/$currentmonths/$currentdate.yaml
 echo get subscribe sucessfully
-mv $currentdate.txt sub_v2rayshare.txt
-mv $currentdate.yaml sub_v2rayshare.yaml
+mv $currentdate.txt sub.txt
+mv $currentdate.yaml sub.yaml
 echo hope you have a good day~
 echo bye~
